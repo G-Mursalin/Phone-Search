b3e0efdd7e3b8aa0fb3c1be8ec9b815fe9e41cbd
Golam Mursalin <golammursalin309@gmail.com>
2022-02-28 23:11:29 +0600

    data loaded

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+// Selectors
+const formContainer = document.querySelector(".form_container");
+
+const formInput = document.querySelector(".form_input");
+
+formContainer.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  loadData(formInput.value.toLowerCase());
+
+  formInput.value = "";
+});
+
+const loadData = async (searchText) => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+  );
+
+  const data = await response.json();
+
+  console.log(data);
+};
