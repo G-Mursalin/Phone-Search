f777b608efd9b26316e71efcd0c17dad9a9a1a26
Golam Mursalin <golammursalin309@gmail.com>
2022-03-01 10:45:27 +0600

    card added

--- js/script.js
+++ js/script.js
@@ -2,9 +2,12 @@
 
 // Selectors
 const formContainer = document.querySelector(".form_container");
-
 const formInput = document.querySelector(".form_input");
-
+const searchMessage1 = document.querySelector(".search_message-1");
+const searchMessage2 = document.querySelector(".search_message-2");
+const numOfResult = document.querySelector(".num_of_result");
+const cardContainer = document.querySelector(".card_container");
+// Handler For formContainer
 formContainer.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -13,12 +16,63 @@ formContainer.addEventListener("submit", (e) => {
   formInput.value = "";
 });
 
+// Load Data From API
 const loadData = async (searchText) => {
+  // Reset
+  cardContainer.textContent = "";
   const response = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
 
-  const data = await response.json();
+  const dataReceived = await response.json();
 
-  console.log(data);
+  showDataUI(dataReceived);
+};
+
+// Data Show In UI
+const showDataUI = (dataReceived) => {
+  const { status, data } = dataReceived;
+
+  if (status === false) {
+    searchMessage2.classList.remove("d-none");
+    searchMessage1.classList.add("d-none");
+    return;
+  }
+  searchMessage1.classList.remove("d-none");
+  searchMessage2.classList.add("d-none");
+  // Set The Find Result Number
+  numOfResult.textContent = data.length;
+
+  if (data.length > 20) {
+    extraDataShow(data.slice(20));
+  }
+  firstTwentyData(data.slice(0, 20));
+};
+
+// First 20 Data Show to UI
+const firstTwentyData = (firstTwentyDataArray) => {
+  console.log(firstTwentyDataArray);
+
+  firstTwentyDataArray.forEach((val) => {
+    const html = `
+    <div class="col">
+    <div class="card p-3">
+      <img src="${val.image}" class="card-img-top w-75 " alt="${val.phone_name}" />
+      <div class="card-body">
+        <h5 class="card-title">${val.phone_name}</h5>
+        <p class="card-text">
+         Brand Name: ${val.brand}
+        </p>
+        <a href="#" class="btn btn-primary">Details</a>
+      </div>
+    </div>
+  </div>
+    `;
+    cardContainer.insertAdjacentHTML("beforeend", html);
+  });
+};
+
+// Extra Data Show to UI
+const extraDataShow = (extraDataArray) => {
+  // console.log(extraDataArray);
 };
