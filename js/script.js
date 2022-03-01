4efabb92e2aca74ecccd8ad492c0c7bf6f70c722
Golam Mursalin <golammursalin309@gmail.com>
2022-03-01 20:19:19 +0600

    all functionality added

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ const chipSetEl = document.querySelector(".chipSet");
 const memoryEl = document.querySelector(".memory");
 const sensorsEl = document.querySelector(".sensors");
 const othersInfoEl = document.querySelector(".othersInfo");
+const btnSeeAll = document.querySelector(".btn_see_all");
 // Handler For formContainer
 formContainer.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -45,6 +46,7 @@ const loadData = async (searchURL) => {
 };
 
 // Data Show In UI
+let extraData;
 const showDataUI = (dataReceived) => {
   const { status, data } = dataReceived;
 
@@ -58,9 +60,9 @@ const showDataUI = (dataReceived) => {
   searchMessage2.classList.add("d-none");
   // Set The Find Result Number
   numOfResult.textContent = data.length;
-
   if (data.length > 20) {
-    extraDataShow(data.slice(20));
+    // extraDataShow(data.slice(20));
+    extraData = data.slice(20);
   }
   firstTwentyData(data.slice(0, 20));
 };
@@ -77,7 +79,6 @@ const showDetailsData = (dataReceived) => {
   const { data } = dataReceived;
   const { name, image, releaseDate, others } = data;
   const { storage, displaySize, chipSet, memory, sensors } = data.mainFeatures;
-  console.log(data);
 
   detailsPhoneName.textContent = name;
   //Set Image Data to UI
@@ -94,58 +95,75 @@ const showDetailsData = (dataReceived) => {
   // Set Storage Data to UI
   setDataUI(storageEl, storage, "Storage");
   // Set Sensors Data to UI
-  sensors.forEach((val) => {
-    const html = `
-     <li>${val}</li>
-    `;
-    sensorsEl.insertAdjacentHTML("beforeend", html);
-  });
-  // Set Others Data to UI
-  const othersDataArray = Object.entries(others);
+  if (sensors !== undefined) {
+    sensors.forEach((val) => {
+      const html = `
+       <li>${val}</li>
+      `;
+      sensorsEl.insertAdjacentHTML("beforeend", html);
+    });
+  } else {
+    setDataUI(sensorsEl, undefined, "Sensors");
+  }
 
-  othersDataArray.forEach((val) => {
-    const html = `
-      <p class="mb-0">${val[0]}:</p>
-      <p class="ms-3">${val[1]}</p>
-    `;
-    othersInfoEl.insertAdjacentHTML("beforeend", html);
-  });
+  // Set Others Data to UI
+  if (others !== undefined) {
+    const othersDataArray = Object.entries(others);
+
+    othersDataArray.forEach((val) => {
+      const html = `
+        <p class="mb-0">${val[0]}:</p>
+        <p class="ms-3">${val[1]}</p>
+      `;
+      othersInfoEl.insertAdjacentHTML("beforeend", html);
+    });
+  } else {
+    setDataUI(othersInfoEl, undefined, "Others");
+  }
+};
+// Creat HTML Template
+const creatHTML = (val) => {
+  return `<div class="col">
+  <div class="card p-3">
+    <img src="${val.image}" class="card-img-top w-75 " alt="${val.phone_name}" />
+    <div class="card-body">
+      <h5 class="card-title">${val.phone_name}</h5>
+      <p class="card-text">
+       Brand Name: ${val.brand}
+      </p>
+      <button
+      type="button"
+      id=${val.slug}
+      class="btn btn_detail btn-primary"
+      data-bs-toggle="modal"
+      data-bs-target="#exampleModal"
+    >
+       Details
+    </button>
+    </div>
+  </div>
+</div>`;
 };
 
 // First 20 Data Show to UI
 const firstTwentyData = (firstTwentyDataArray) => {
   // Reset
   cardContainer.textContent = "";
-  console.log(firstTwentyDataArray);
-
+  let createdHtml = "";
   firstTwentyDataArray.forEach((val) => {
-    const html = `
-    <div class="col">
-    <div class="card p-3">
-      <img src="${val.image}" class="card-img-top w-75 " alt="${val.phone_name}" />
-      <div class="card-body">
-        <h5 class="card-title">${val.phone_name}</h5>
-        <p class="card-text">
-         Brand Name: ${val.brand}
-        </p>
-        <button
-        type="button"
-        id=${val.slug}
-        class="btn btn_detail btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-      >
-         Details
-      </button>
-      </div>
-    </div>
-  </div>
-    `;
-    cardContainer.insertAdjacentHTML("beforeend", html);
+    createdHtml += creatHTML(val);
   });
+  cardContainer.insertAdjacentHTML("beforeend", createdHtml);
+  btnSeeAll.classList.toggle("d-none");
 };
 
-// Extra Data Show to UI
-const extraDataShow = (extraDataArray) => {
-  // console.log(extraDataArray);
-};
+// Show All Data
+btnSeeAll.addEventListener("click", () => {
+  let createdHtml = "";
+  extraData.forEach((val) => {
+    createdHtml += creatHTML(val);
+  });
+
+  cardContainer.insertAdjacentHTML("beforeend", createdHtml);
+  btnSeeAll.classList.toggle("d-none");
+});
